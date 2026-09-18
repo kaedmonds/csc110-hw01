63127d3584b4da783ff4f5492f45e2b8a6e0b6a3
Kathryn Edmonds <kaedmonds@smith.edu>
2026-09-18 15:56:49 -0400

    hw01.py

--- hw01.py
+++ hw01.py
@@ -45,6 +45,22 @@ def main():
     # Part 1: Basic Operations
     # =============================================
     # Your code for part 1 under this line and before the print statements
+x=27
+y=1
+a=1.5
+b=7
+c=-1
+result1=(3*x - 9*y) / (2*a * (b - c))
+
+print("Part 1: x = 27")
+print("Part 1: y = 1")
+print("Part 1: a = 1.5")
+print("Part 1: b = 7")
+print("Part 1: c = -1")
+print(f"Part 1: result = {result1}")
+
+
+
 
     # End of Part 1 ----------------------
 
