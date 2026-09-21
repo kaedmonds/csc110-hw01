fece609d11e61b7a32d85fd99c1d3373ea812442
Kathryn Edmonds <kaedmonds@smith.edu>
2026-09-21 11:21:05 -0400

    Commit 2

--- hw01.py
+++ hw01.py
@@ -1,5 +1,5 @@
 # ------------------------------------------------------
-#        Name: (put your name here)
+#        Name: Kathryn Edmonds
 #       Peers: (add any collaborators)
 #  References: (anything you checked to solve this)
 # ------------------------------------------------------
@@ -45,19 +45,19 @@ def main():
     # Part 1: Basic Operations
     # =============================================
     # Your code for part 1 under this line and before the print statements
-x=27
-y=1
-a=1.5
-b=7
-c=-1
-result1=(3*x - 9*y) / (2*a * (b - c))
+    x=27
+    y=1
+    a=1.5
+    b=7
+    c=-1
+    result1=(3*x - 9*y) / (2*a * (b - c))
 
-print("Part 1: x = 27")
-print("Part 1: y = 1")
-print("Part 1: a = 1.5")
-print("Part 1: b = 7")
-print("Part 1: c = -1")
-print(f"Part 1: result = {result1}")
+    print("Part 1: x =", x)
+    print("Part 1: y =", y)
+    print("Part 1: a =", a)
+    print("Part 1: b =", b)
+    print("Part 1: c =", c)
+    print("Part 1: result =", result1)
 
 
 
@@ -68,12 +68,12 @@ print(f"Part 1: result = {result1}")
     # Part 2: Power
     # =============================================
     # Your code for part 2 under this line and before the print statements
-x=5
-y=-3
-result2=x**2*y**4
-print("Part 2: x = 5")
-print("Part 2: y = -3")
-print(f"Part 2: result = {result2}")
+    x=5
+    y=-3
+    result2=x**2*y**4
+    print("Part 2: x =", x)
+    print("Part 2: y =", y)
+    print("Part 2: result =", result2)
 
     # End of Part 2 ----------------------
 
@@ -83,6 +83,12 @@ print(f"Part 2: result = {result2}")
     # =============================================
     # Your code for part 3 under this line and before the print statements
 
+    a=100
+    b=13
+    result3=a//b
+    print("Part 3: a =", a)
+    print("Part 3: b =", b)
+    print("Part 3: result =", result3)
     # End of Part 3 ----------------------
 
 
@@ -90,6 +96,8 @@ print(f"Part 2: result = {result2}")
     # =============================================
     # Your code for part 4 under this line and before the print statements
 
+    result4=a%b
+    print("Part 4: result =", result4)
     # End of Part 4 ----------------------
 
 if __name__ == "__main__":
